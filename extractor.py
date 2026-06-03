ea6df53a3669c6e503d719bbcc2af4a6436bbe02
NK2248 <111104611+NK2248@users.noreply.github.com>
2026-06-03 16:42:48 +0700

    change file name from [Standard-2025] IO Point Lists for 115 kV SCPS H Scheme.xlsx to [Standard-2025] IO Point Lists for 115 kV SCPS Main & Transfer.xlsx in extractor.py, and remove CENTER_01 from index.html. Also, update table styles in index.html to allow horizontal resizing of columns and prevent text overflow.

--- extractor.py
+++ extractor.py
@@ -16,7 +16,7 @@ except ImportError:
     sys.exit(1)
 
 # ── Config ────────────────────────────────────────────────────────────────────
-SOURCE_FILE = r"C:\CU\EIC\Intern\Feeder_Checker\OG\Substation\[Standard-2025] IO Point Lists for 115 kV SCPS H Scheme.xlsx"
+SOURCE_FILE = r"C:\CU\EIC\Intern\Feeder_Checker\OG\Substation\[Standard-2025] IO Point Lists for 115 kV SCPS Main & Transfer.xlsx"
 OUTPUT_DIR  = r"C:\CU\EIC\Intern\Feeder_Checker\Substation_csv"
 # ─────────────────────────────────────────────────────────────────────────────
 
